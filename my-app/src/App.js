ff5b21e9a8fa852d2fba8e792c929d532e49d46e
jrobles <jrobles@eecs.wsu.edu>
2023-01-22 22:37:22 -0800

    added react functionalities

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,76 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [merchants, setMerchants] = useState(false)
+  
+  useEffect(() => {
+    getMerchant();
+  }, [])
+
+  /*
+    Get merchants from the database. NodeJS hosts the database on localhost 3001 so we need to 
+    make requests to that URL
+  */
+  function getMerchant() {
+    fetch('http://localhost:3001') // this is where node is ran
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      setMerchants(data)
+    })
+  }
+
+  /*
+    Allow for user input to insert new merchant into DB. We are sending info to server -> POST verb, app.post() in node's index.js
+    We will be sending JSON information to the database
+  */
+  function createMerchant() {
+    let name = prompt('Enter merchant name')
+    let email = prompt('Enter merchant email')
+    fetch('http://localhost:3001/merchants', {
+      method: 'POST', 
+      headers: {
+        'Content-type': 'application/json', 
+      },
+      body: JSON.stringify({name, email})
+    })
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      alert(data)
+      getMerchant()
+    })
+  }
+
+  function deleteMerchant() {
+    let id = prompt('Enter merchant id to be deleted')
+    fetch('http://localhose3001/merchants/${id}', {
+      method: 'DELETE', 
+    })
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      alert(data)
+      getMerchant()
+    })
+  }
+
   return (
     <div>
       <h1>My app</h1>
+      <div>
+        {merchants ? merchants : 'There is no merchant data available'}
+        <br />
+        <button onClick={createMerchant}>Add merchant</button>
+        <br />
+        <button onClick={deleteMerchant}>Delete merchant</button>
+        <br />
+        <button onClick={getMerchant}> Get merchants </button>
+      </div>
     </div>
   );
 }
--- node-postgres/index.js
+++ node-postgres/index.js
@@ -12,7 +12,7 @@ const merchant_model = require('./merchant_model')
 
 app.use(express.json())             // Allow for incoming reqs with JSON payloads
 app.use(function (req, res, next) { // Allow for requests to this app from React
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //3001? 
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //3000 is the react localhost
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
